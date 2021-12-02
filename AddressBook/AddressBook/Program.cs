91261b0cd8b9a5e016223119dfeb6d4f195aaae6
sanajylopis25 <sanajylopis25@gmail.com>
2021-12-02 22:47:47 +0530

    First Commit

---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AddressBook
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Addresses { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string email { get; set; }
+        static void Main(string[] args)
+        {
+            
+            int command = 0;
+            while (command != 4)
+            {
+                Console.Clear();
+                Console.WriteLine("1: Add \n2: Remove \n3: List \n4: exit");
+                Console.WriteLine("Please enter a command number: ");
+                command = Convert.ToInt32(Console.ReadLine());
+                switch (command)
+                {
+                    case 1:
+                        AddPeople.AddPerson();
+                        break;
+                    case 2:
+                        RemovingPeople.RemovePerson();
+                        break;
+                    case 3:
+                        ListingPeople.ListPeople();
+                        break;
+                }
+            }
+        }
+        
+    }
+}
